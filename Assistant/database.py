1ccefb2d4519bbcadd067c488d8b4718a2901cdb
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-09-05 00:35:32 +0300

    type error found

--- Assistant/database.py
+++ Assistant/database.py
@@ -42,6 +42,7 @@ def get_scraped_data(_id:int):
 
 def save_property_info(_id:int,key:str,value):
     property.update_one({"_id":_id,},{"$set":{key:value}},upsert=True)
+    
 def set_current_property(_id:int,room_id:int = None):
     Users.update_one({"_id":_id},{"$set":{"current_property":room_id}},upsert=True)
 
