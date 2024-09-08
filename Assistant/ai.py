3b5f9f5402b8f1b27db89f46d44c5aef67c8bede
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-09-08 12:00:21 +0300

    typo

--- Assistant/ai.py
+++ Assistant/ai.py
@@ -133,7 +133,7 @@ class llm:
                 try:
                     amenity = self.function_information[f"{self.current_property_id}"]['aminities']
                     print(f"found amenity:{amenity}")
-                    return {"function_response":amenity.keys(),"image":None}
+                    return {"function_response":str(amenity.keys()),"image":None}
                 except:
                     return {"function_response":"Error: amenity not found.","image":None}
 
